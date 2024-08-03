3a795ecaf84350e1008bf1042cce393c346f2c6f
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-08-03 18:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If requi
-const element=document.getElementById('level');
+window.onload = function() {
+    const element = document.getElementById('level');
     var level = 0;
     while (element) {
         level++;
         element = element.parentElement;
     }    
     alert("The level of the element is: "+ level);
-
+}
 
